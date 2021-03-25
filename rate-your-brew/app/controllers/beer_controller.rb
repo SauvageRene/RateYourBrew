371ab69a8a8d73c63f1202c1e90374266299bc43
Rene <renegarcia1864@gmail.com>
2021-03-25 12:33:12 -0500

    added Get/Post routes

--- rate-your-brew/app/controllers/beer_controller.rb
+++ rate-your-brew/app/controllers/beer_controller.rb
@@ -3,5 +3,21 @@ class BeerController < ApplicationController
     #Create
 
     get '/beers/new' do 
+        erb :'/orders/new'
     end
+    #Create a new beer and save to the DB
+    post '/beers' do
+        @beer = Beer.create(
+            name: params[:name], 
+            description: params[:description], 
+            ABVpercentage: params[ABV], 
+            rating: params[:rating])
+
+        redirect "orders/#{@beer.id}"
+    end
+
+    get 'orders/:id' do
+        @order = Order.find(params[:id])
+
+        erb :'/beers/show'
 end
