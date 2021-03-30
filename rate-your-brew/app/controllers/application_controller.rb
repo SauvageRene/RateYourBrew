ed8454ce5fc9315b281b573c7682115460a80021
Rene <renegarcia1864@gmail.com>
2021-03-30 17:19:34 -0500

    Logged in?

--- rate-your-brew/app/controllers/application_controller.rb
+++ rate-your-brew/app/controllers/application_controller.rb
@@ -19,4 +19,10 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by_id(session["user_id"])
     end
 
+    def logged_in?
+      !!current_user
+    end
+
+    
+
 end
